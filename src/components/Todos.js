7e8600517e4ca7beca00b441b2fc1b25b95583be
shamimaldeen <shamimaldeen@gmail.com>
2024-05-11 00:37:05 +0600

    React-redux -todo app API fetching

---
+++ src/components/Todos.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllTodos } from "../services/actions/todosAction";
+
+const Todos = () => {
+  const { isLoading, todos, error } = useSelector((state) => state);
+
+  console.log(isLoading);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllTodos());
+  }, []);
+
+  return (
+    <div>
+      <h2>Todos App</h2>
+      {isLoading && <h3>Loading ...</h3>}
+      {error && <h3>{error.message}</h3>}
+      <section>
+        {todos &&
+          todos.map((todo) => {
+            return (
+              <article key={todo.id}>
+                <h4>{todo.id}</h4>
+                <p>{todo.title}</p>
+              </article>
+            );
+          })}
+      </section>
+    </div>
+  );
+};
+export default Todos;
---
+++ src/services/actions/todosAction.js
@@ -0,0 +1,17 @@
+import {
+    GET_TODOS_FAILED,
+    GET_TODOS_REQUEST,
+    GET_TODOS_SUCCESS,
+  } from "../constants/todosConstant";
+  
+  import axios from "axios";
+  
+  export const getAllTodos = () => async (dispatch) => {
+    dispatch({ type: GET_TODOS_REQUEST });
+    try {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      dispatch({ type: GET_TODOS_SUCCESS, payload: res.data });
+    } catch (error) {
+      dispatch({ type: GET_TODOS_FAILED, payload: error.message });
+    }
+  };
